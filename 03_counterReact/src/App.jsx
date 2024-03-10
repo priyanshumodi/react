57185a3049459d866e6ebb49156a632f179cd97b
Priyanshu Modi <70794004+priyanshumodi@users.noreply.github.com>
2024-03-10 17:25:10 +0000

    counter project

--- 03_counterReact/src/App.jsx
+++ 03_counterReact/src/App.jsx
@@ -6,15 +6,32 @@ import './App.css'
 
 function App() {
 
-  let counter = 15
+  let [counter,setCounter] = useState(15);
+
+  // let counter = 15
+
+  const addValue = () => {
+    if(counter<20)
+    {
+      console.log(`value added ${counter}`)
+      setCounter(counter+1)
+    }
+  }
+  const removeValue = () => {
+    if(counter>0)
+    {
+      console.log(`value remove ${counter}`)
+      setCounter(counter-1)
+    }
+  }
   return (
     <>
       <h1>Jai Shree Krishna</h1>
       <h2>counter value : {counter}</h2>
-      <button>Add value {counter}</button>
+      <button onClick={addValue}>Add value {counter}</button>
       <br />
-      <button>Remove value {counter}</button>
-      <footer>footer {counter}</footer>
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>footer {counter}</p>
     </>
   )
 }
