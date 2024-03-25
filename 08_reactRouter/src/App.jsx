4e3ac6df6aa996ba4e4dedebb332d732bb243bc4
Priyanshu Modi <70794004+priyanshumodi@users.noreply.github.com>
2024-03-25 12:08:47 +0000

    react router

--- 08_reactRouter/src/App.jsx
+++ 08_reactRouter/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-yellow-400 text-white text-4xl p-4'>Jai shree krishna holika ki hardik shubhkamnaye</h1>
+      <Header/>
     </>
   )
 }
