313c14b0fcdb5c8472eab117c949b3729d99604d
Priyanshu Modi <70794004+priyanshumodi@users.noreply.github.com>
2024-04-04 18:57:38 +0000

    auth

--- 13_megaBlog/src/appwrite/auth.js
+++ 13_megaBlog/src/appwrite/auth.js
@@ -12,6 +12,46 @@ export class AuthService {
 
         this.account = new Account(this.client);
     }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(),email,password,name)
+            if(userAccount) {
+                // call another method
+                return this.login({email,password})
+            }
+            else {
+                return userAccount;
+            }
+            
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({email,password}) {
+        try {
+            return await this.account.createEmailSession(email,password)            
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
 }
 
 const authservice = new AuthService();
