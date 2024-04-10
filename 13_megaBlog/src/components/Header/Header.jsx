3f546cbc900a6ec7c537a85b10a91e18c39f7600
Priyanshu Modi <70794004+priyanshumodi@users.noreply.github.com>
2024-04-10 10:38:43 +0000

    krdo

--- 13_megaBlog/src/components/Header/Header.jsx
+++ 13_megaBlog/src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-
+import {Container, Logo, LogoutBtn} from '../index'
+import { Link } from "react-router-dom";
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
 function Header() {
+    const authStatus = useSelector(state => state.status)
     return (
        <div>
         jai shree ram
